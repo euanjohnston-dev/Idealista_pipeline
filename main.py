83d64d9ff9c52a585e60de2acd7663014edfd448
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-11-15 14:53:37 +0100

    ammended to function for pub/sub

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from idealista import idealista_run
+import base64
 
-def pipeline_idealista(request):
+def pipeline_idealista(data, context):
+  
+  message = base64.b64decode(data['data']).decode('utf-8')
+  print(f'Hello, {message}!')
   idealista_run()
   return "Pipeline run successfully!"
