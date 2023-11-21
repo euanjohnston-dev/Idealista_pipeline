2f58a334dcb3e68868562cf1cbd69d7939447112
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-11-21 09:59:42 +0100

    Fixed error with slack messaging

--- slack_messaging.py
+++ slack_messaging.py
@@ -2,7 +2,6 @@ import slack
 import os
 
 def setup_slack_client():
-    # No need to load .env, as Cloud Functions manages environment variables directly
     return slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 def send_slack_message(message, channel='#test', error_message=None):
@@ -13,12 +12,10 @@ def send_slack_message(message, channel='#test', error_message=None):
 
     client.chat_postMessage(channel=channel, text=message)
 
-def pipeline_success(request):
-    # Adjust the function signature to handle HTTP request, if needed
+def pipeline_success():
     send_slack_message('Pipeline ran successfully')
     return "Pipeline run successfully!"
 
-def pipeline_failure(request, error_message=None):
-    # Adjust the function signature to handle HTTP request, if needed
+def pipeline_failure(error_message):
     send_slack_message('Pipeline failed', error_message=error_message)
     return f"Pipeline failed with error: {error_message}" if error_message else "Pipeline failed"
